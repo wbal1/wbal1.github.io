95582576a03a3a9e157fe75fa18c778bf553689a
wbal1 <adult1818@outlook.kr>
2024-04-19 12:44:41 +0900

    Update 240419
    많은 것을 함

---
+++ ruffle/ruffle-2.js
@@ -0,0 +1,17 @@
+var swfobject = {};
+
+swfobject.embedSWF = function(url, cont, width, height){
+	var ruffle = window.RufflePlayer.newest(),
+		player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
+			width: width,
+			height: height,
+			style: 'width: ' + width + 'px; height: ' + height + 'px',
+		}
+	);
+	
+	player.load({ url: url });
+}
+
+window.RufflePlayer.config = {
+	autoplay: "on"
+};
